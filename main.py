490c243b7b393aa77e87c0eefd77900952456cc5
Diodus <timofey.ivanov07@mail.ru>
2026-01-22 16:15:37 +0400

    Add logging
    Logging of user actions has been added.

--- main.py
+++ main.py
@@ -1,11 +1,20 @@
 import sqlite3
+import sys
+
 import telebot
+import logging
 from config import TOKEN
 
 bot = telebot.TeleBot(TOKEN)
+logging.basicConfig(level=logging.INFO,
+                    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
+                    handlers=[logging.FileHandler('bot.log', encoding="utf-8")])
+logger = logging.getLogger(__name__)
+
 
 @bot.message_handler(commands=['help'])
 def command_help(message):
+    logger.info(f"User {message.from_user.id} selected /help. ")
     bot.send_message(message.chat.id,text="/start - launching the bot. \n"
                                           "/register - updating user data. \n"
                                           "/check - checking how the user is logged into the database.\n"
@@ -20,15 +29,16 @@ def proverka_bd():
                 UserName TEXT
             )''')
             conn.commit()
+            logger.info(f"Database created/checked successfully")
     except Exception as e:
-        print(f"Error when creating the table: {e}")
+        logger.info(f"Database error: {e}")
 
 
 @bot.message_handler(commands=['start'])
 def start(message):
+    logger.info(f"User {message.from_user.id} selected /start. ")
     user_id = str(message.from_user.id)
 
-    # Проверка регистрации пользователя
     with sqlite3.connect('Users.db') as conn:
         cursor = conn.cursor()
         cursor.execute("SELECT * FROM users WHERE UserId = ?", (user_id,))
@@ -37,7 +47,6 @@ def start(message):
     if user:
         bot.send_message(message.chat.id, "You are already registered!")
     else:
-        # Автоматическая регистрация при /start
         with sqlite3.connect('Users.db') as conn:
             conn.execute(
                 "INSERT INTO users (UserId, firstname, lastname, UserName) VALUES (?, ?, ?, ?)",
@@ -49,6 +58,7 @@ def start(message):
                 )
             )
             conn.commit()
+        logger.info(f"User {message.from_user.id} registered successfully")
         bot.send_message(message.chat.id, "You are already registered!")
 
     bot.send_message(
@@ -60,13 +70,15 @@ def start(message):
 
 @bot.message_handler(commands=['register'])
 def register(message):
+    logger.info(f"User {message.from_user.id} selected /register. ")
     bot.send_message(message.chat.id, "Enter your name:")
+
     bot.register_next_step_handler(message, process_firstname)
 
 
 def process_firstname(message):
     firstname = str(message.text.strip())
-
+    logger.info(f"User {message.from_user.id} enter his name")
     if not firstname:
         bot.send_message(message.chat.id, "Name can't be empty. Try again:")
         bot.register_next_step_handler(message, process_firstname, )
@@ -78,7 +90,7 @@ def process_firstname(message):
 
 def process_lastname(message, firstname):
     lastname = (message.text.strip())
-
+    logger.info(f"User {message.from_user.id} enter his lastname")
     if not lastname:
         bot.send_message(message.chat.id, "Lastname can't be empty. Try again:")
         bot.register_next_step_handler(message, process_lastname,firstname)
@@ -97,6 +109,7 @@ def process_lastname(message, firstname):
             conn.commit()
 
         bot.send_message(message.chat.id, "Data was updated successfully!")
+        logger.info(f"User's data updated successfully")
     except Exception as e:
         bot.send_message(message.chat.id, f"Error of saving: {e}")
 
@@ -104,7 +117,7 @@ def process_lastname(message, firstname):
 @bot.message_handler(commands=['check'])
 def check_registration(message):
     user_id = str(message.from_user.id)
-
+    logger.info(f"User {message.from_user.id} selected /check.")
     with sqlite3.connect('Users.db') as conn:
         cursor = conn.cursor()
         cursor.execute("SELECT firstname, lastname, UserName FROM users WHERE UserId = ?", (user_id,))
@@ -118,9 +131,10 @@ def check_registration(message):
             f"Username: {user[2] or 'empty'}"
         )
         bot.send_message(message.chat.id, response)
+        logger.info(f"User's data was printed.")
     else:
         bot.send_message(message.chat.id, "You are not registered. Use /start or /register.")
-
+        logger.info(f"User {message.from_user.id} not registered.")
 
 if __name__ == '__main__':
     proverka_bd()
